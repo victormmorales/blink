fe527b7f007417d93ed4dffc48999ed00b00d019
victormmorales <victorfasrev@gmail.com>
2022-01-04 17:19:13 +0100

    ejercicio 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import JavaScript from './components/JavaScript';
+import logo from './assets/img/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="prueba-container">
+      <div className='logo'>
+        <h1>Prueba técnica</h1>
+        <img src={logo} alt="logo" />
+      </div>
+      <div>
+        <JavaScript />
+      </div>
     </div>
   );
 }
---
+++ src/components/JavaScript.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+export default function JavaScript() {
+
+    const [ resultado, setResultado ] = useState(0);
+
+    // Comprobacion si es un numero
+    function isNum(numero){
+        return !isNaN(numero)
+    }
+    
+    let lista1 = [1, 4, 3, 0];
+    let lista2 = [1, 2, [5, 8], 4];
+    let lista3 = [0, [6, 2], null, 7, 1];
+
+    const sumArray = (array) => {
+
+        let total = 0;
+
+        //validar si es un array
+        if(Array.isArray(array)){
+            for(let i = 0; i < array.length; i++) {
+                const elemento = array[i];
+                //validacion si es un numero
+                if(isNum(elemento)){
+                    total += elemento;
+                }
+            } 
+        } else {
+            console.error('No es un array');
+        };
+
+        return setResultado(total);
+    };
+
+    return (
+        <div className='suma-principal'>
+            <h2>1. JavaScript</h2>
+            <div className='suma-container'>
+                <div className='operaciones'>
+                    <p>Lista1: 1, 4, 3, 0</p>
+                    <button onClick={() => sumArray(lista1)}>Sumar</button>
+                </div>
+
+                <div className='operaciones'>
+                    <p>Lista2: 1, 2, [5, 8], 4</p>
+                    <button onClick={() => sumArray(lista2)}>Sumar</button>
+                </div>
+
+                <div className='operaciones'>
+                    <p>Lista3: 0, [6, 2], null, 7, 1</p>
+                    <button onClick={() => sumArray(lista3)}>Sumar</button>
+                </div>
+            </div>
+            <div className='suma-resultado'>
+                <p>Resultado = {resultado}</p>
+                <button onClick={ () => setResultado(0)}>Reiniciar</button>
+            </div>
+        </div>
+    )
+}
