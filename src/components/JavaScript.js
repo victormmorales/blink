a438733d6cc16245f6e8b5d9058218e015df6e35
victormmorales <victorfasrev@gmail.com>
2022-01-07 12:17:52 +0100

    js

--- src/components/JavaScript.js
+++ src/components/JavaScript.js
@@ -12,28 +12,39 @@ export default function JavaScript() {
   let lista2 = [1, 2, [5, 8], 4];
   let lista3 = [0, [6, 2], null, 7, 1];
 
-  const sumArray = (array) => {
+  const sumArray2 = (array) => {
     let total = 0;
+
     let totalArray = 0;
+    let totalSubArray = 0;
 
-    //validar si es un array
-    if (Array.isArray(array)) {
-      for (let i = 0; i < array.length; i++) {
-        const elemento = array[i];
-        //validacion si es un numero
-        if (isNum(elemento)) {
-          total += elemento;
+    if(Array.isArray(array)){
+        for(let i = 0; i < array.length; i++) {
+            if(Array.isArray(array[i])){
+                const elemento = array[i];
+                for(let j = 0; j < elemento.length; j++) {
+                    const elementoArray = elemento[j];
+
+                    //validacion si es un numero
+                    if(isNum(elementoArray)){
+                        totalSubArray += elementoArray;
+                    }
+                }
+            }
+
+            const elemento = array[i];
+            //validacion si es un numero
+            if (isNum(elemento)) {
+                totalArray += elemento;
+            }
         }
-      }
+        total = totalArray + totalSubArray;
     } else {
-      console.error("No es un array");
+        return '¡No es un Array!';
     }
-    return setResultado(total);
-  };
 
-  // const reducer = (a, b) => a + b;
-  // console.log(lista2.reduce(reducer));
-  console.log("REDUCER", lista2.reduce((a, b) => a + b));
+    return setResultado(total);
+  }
 
   return (
     <div className="suma-principal">
@@ -42,17 +53,17 @@ export default function JavaScript() {
       <div className="suma-container">
         <div className="operaciones">
           <p>Lista1: 1, 4, 3, 0</p>
-          <button onClick={() => sumArray(lista1)}>Sumar</button>
+          <button onClick={() => sumArray2(lista1)}>Sumar</button>
         </div>
 
         <div className="operaciones">
           <p>Lista2: 1, 2, [5, 8], 4</p>
-          <button onClick={() => sumArray(lista2)}>Sumar</button>
+          <button onClick={() => sumArray2(lista2)}>Sumar</button>
         </div>
 
         <div className="operaciones">
           <p>Lista3: 0, [6, 2], null, 7, 1</p>
-          <button onClick={() => sumArray(lista3)}>Sumar</button>
+          <button onClick={() => sumArray2(lista3)}>Sumar</button>
         </div>
       </div>
       <div className="suma-resultado">
